18d03621dc19760abe9930f3101012117dd1dd36
Sunil Kumawat <sunilkumawat1255@gmail.com>
2025-03-10 10:15:50 +0530

    build error resolve

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Make sure your build directory is named 'build' (for Vercel)
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'; // Create a separate chunk for node_modules
-          }
-          // You can add additional chunking logic here
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000, // Increase the warning size limit to 1000 KB (1MB)
-  }
-});
+})
